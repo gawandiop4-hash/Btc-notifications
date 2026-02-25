6a24aabbe512b6b29e7138267b91099c70198e36
gawandiop4-hash <gawandiop4@gmail.com>
2026-02-25 21:49:30 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.matchAll({ type: 'window' }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      if ('focus' in clientList[i]) return clientList[i].focus();
+    }
+    if (clients.openWindow) return clients.openWindow('/');
+  }));
+});
+
+self.addEventListener('push', function(event) {});
